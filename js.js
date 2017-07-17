3bd41aaf133b51f0604027c5fa8e94098637ec40
World_Languages <mundofinky@gmail.com>
2017-07-17 14:51:25 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
-document.getElementById("author").innerHTML += " - Animated thumbnail script ready";
+document.getElementById("author").innerHTML += " - <b><div id='animatedThumbnailText' style='display:inline;'></div></b>";
+document.getElementById("animatedThumbnailText").innerHTML = "<font color='blue'>Animated thumbnail script ready</font>";
 var dropper = $("#info");
 dropper.on("dragover", function(e) {
 	e.stopPropagation();
@@ -11,6 +12,7 @@ dropper.on("drop", function(e) {
 	file = e.originalEvent.dataTransfer.files[0];
 	var reader = new FileReader();
 	reader.onload = function(e2) {
+document.getElementById("animatedThumbnailText").innerHTML = "<font color='orange'>Uploading thumbnail...</font>";
 		$.ajax({
 			type: "POST",
 			url: "/internalapi/project/thumbnail/"+Scratch.INIT_DATA.PROJECT.model.id+"/set/",
@@ -18,9 +20,12 @@ dropper.on("drop", function(e) {
 			contentType: "",
 			processData: false,
 			success: function(msg) {
-				alert("Server response: " + msg.status);
-			}
+if(msg.status == "ok"){
+document.getElementById("animatedThumbnailText").innerHTML = "<font color='green'>Success</font>";} else{
+document.getElementById("animatedThumbnailText").innerHTML = "<font color='red'>Error</font>";
+}
+            }
 		});
-	}
+	};
 	reader.readAsArrayBuffer(file);
 });
